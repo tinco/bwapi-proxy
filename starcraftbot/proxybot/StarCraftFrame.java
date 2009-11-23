7e6c3c1350135d2f8527ad439cb716b8d9b9f122
fischp@gmail.com <fischp@gmail.com@434c508a-d61b-11de-901b-a378632c6355>
2009-11-23 07:34:16 +0000

    draws vision and attack range circles around your own units
    git-svn-id: https://bwapi-proxy.googlecode.com/svn/trunk@31 434c508a-d61b-11de-901b-a378632c6355

--- starcraftbot/proxybot/StarCraftFrame.java
+++ starcraftbot/proxybot/StarCraftFrame.java
@@ -31,16 +31,16 @@ public class StarCraftFrame extends JPanel implements KeyListener {
 
 	/** draw the starting locations */
 	private static boolean drawStartSpots = true;
-	
+
 	/** draw mineral and gas locations */
 	private static boolean drawResources = true;
 
 	/** reference to the proxy bot */
 	private Game game;
-	
+
 	/** pixels per map tile, StarCraft is 32 */
-	private int tileSize = 4;
-	
+	private int tileSize = 10;
+
 	/** height of the resource panel */
 	private int panelHeight = 30;
 
@@ -49,16 +49,16 @@ public class StarCraftFrame extends JPanel implements KeyListener {
 
 	/** the parent of this component */
 	private JFrame frame;
-	
+
 	/** draw the influence map? */
 	private boolean influenceMap = false;
-	
+
 	/**
 	 * Constructs a JFrame and draws the ProxyBot's state.
 	 */
 	public StarCraftFrame(Game game) {
 		this.game = game;
-		
+
 		int width = game.getMap().getMapWidth();
 		int height = game.getMap().getMapHeight();
 		setPreferredSize(new Dimension(tileSize*width, tileSize*height + panelHeight));
@@ -68,16 +68,16 @@ public class StarCraftFrame extends JPanel implements KeyListener {
 		frame.pack();
 		frame.setLocation(640, 0);
 		frame.addKeyListener(this);
-		frame.setVisible(true);		
+		frame.setVisible(true);
 	}
-	
+
 	/**
 	 * Shuts down the GUI.
 	 */
 	public void stop() {
 		frame.setVisible(false);
 	}
-	
+
 	/**
 	 * Draws the agent's view of the game state.
 	 */
@@ -85,7 +85,7 @@ public class StarCraftFrame extends JPanel implements KeyListener {
 
 		// regular panel
 		if (!influenceMap) {
-			
+
 			// tile set
 			MapWME map = game.getMap();
 			for (int y=0; y<map.getMapHeight(); y++) {
@@ -93,85 +93,97 @@ public class StarCraftFrame extends JPanel implements KeyListener {
 					int walkable = map.isWalkable(x, y) ? 1 : 0;
 					int buildable = map.isBuildable(x, y) ? 1 : 0;
 					int height = map.getHeight(x, y);
-					
+
 					int c = (70*walkable + 60*buildable + 50*height);
 					g.setColor(new Color(c,c,(int)(c*3/4)));
 					g.fillRect(x*tileSize, panelHeight  + y*tileSize, tileSize, tileSize);
 				}
 			}
-	
+
 			// Starting locations
 			if (drawStartSpots) {
 				g.setColor(Color.ORANGE);
 				for (StartingLocationWME location : game.getStartingLocations()) {
-					g.fillRect(location.getX()*tileSize, panelHeight  + location.getY()*tileSize, 4*tileSize, 3*tileSize);				
+					g.fillRect(location.getX()*tileSize, panelHeight  + location.getY()*tileSize, 4*tileSize, 3*tileSize);
 				}
 			}
-			
+
 			// minerals
 			if (drawResources) {
 				g.setColor(new Color(0,255,255));
 				for (MineralWME unit : game.getMinerals()) {
-						g.fillRect(unit.getX()*tileSize, panelHeight  + unit.getY()*tileSize, tileSize, tileSize);				
+						g.fillRect(unit.getX()*tileSize, panelHeight  + unit.getY()*tileSize, tileSize, tileSize);
 				}
 			}
-			
+
 			// gas
 			if (drawResources) {
 				g.setColor(new Color(0,128,0));
 				for (GeyserWME unit : game.getGeysers()) {
-					g.fillRect(unit.getX()*tileSize, panelHeight  + unit.getY()*tileSize, 
-							unit.getType().getTileWidth()*tileSize, unit.getType().getTileHeight()*tileSize);				
+					g.fillRect(unit.getX()*tileSize, panelHeight  + unit.getY()*tileSize,
+							unit.getType().getTileWidth()*tileSize, unit.getType().getTileHeight()*tileSize);
 				}
 			}
-			
+
 			// enemy units
 			g.setColor(new Color(255,0,0));
 			for (EnemyUnitWME unit : game.getEnemyUnits()) {
-				g.fillRect(unit.getX()*tileSize, panelHeight  + unit.getY()*tileSize, 
-						unit.getType().getTileWidth()*tileSize, unit.getType().getTileHeight()*tileSize);				
-			}		
-			
+				g.fillRect(unit.getX()*tileSize, panelHeight  + unit.getY()*tileSize,
+						unit.getType().getTileWidth()*tileSize, unit.getType().getTileHeight()*tileSize);
+			}
+
 			// ally units
 			g.setColor(Color.YELLOW);
 			for (AllyUnitWME unit : game.getAllyUnits()) {
-				g.fillRect(unit.getX()*tileSize, panelHeight  + unit.getY()*tileSize, 
-						unit.getType().getTileWidth()*tileSize, unit.getType().getTileHeight()*tileSize);				
-			}	
-			
-			// player units
-			g.setColor(new Color(0,255,0));
-			for (PlayerUnitWME unit : game.getPlayerUnits()) {
-				g.fillRect(unit.getX()*tileSize, panelHeight  + unit.getY()*tileSize, 
-						unit.getType().getTileWidth()*tileSize, unit.getType().getTileHeight()*tileSize);				
+				g.fillRect(unit.getX()*tileSize, panelHeight  + unit.getY()*tileSize,
+						unit.getType().getTileWidth()*tileSize, unit.getType().getTileHeight()*tileSize);
 			}
-			
+
+			// player units
+			for (PlayerUnitWME unit : game.getPlayerUnits()) {
+				g.setColor(new Color(0,255,0));
+				g.fillRect(unit.getX()*tileSize, panelHeight  + unit.getY()*tileSize,
+						unit.getType().getTileWidth()*tileSize, unit.getType().getTileHeight()*tileSize);
+
+			//vision range
+				g.setColor(new Color(0,0,255));
+				g.drawOval(unit.getX()*tileSize + unit.getType().getTileWidth()*tileSize/2 - unit.getType().getSightRange()*tileSize,
+						  panelHeight  + unit.getY()*tileSize + unit.getType().getTileHeight()*tileSize/2 - unit.getType().getSightRange()*tileSize,
+						  unit.getType().getSightRange()*tileSize * 2, unit.getType().getSightRange()*tileSize * 2);
+
+			//attack range
+				g.setColor(new Color(255,0,0));
+				g.drawOval(unit.getX()*tileSize + unit.getType().getTileWidth()*tileSize/2 - unit.getType().getGroundMaxRange()/2,
+						  panelHeight  + unit.getY()*tileSize + unit.getType().getTileHeight()*tileSize/2 - unit.getType().getGroundMaxRange()/2,
+						  unit.getType().getGroundMaxRange(), unit.getType().getGroundMaxRange());
+			}
+
 			// unit IDs
 			if (drawIDs) {
 				g.setColor(new Color(255,255,255));
 				g.setFont(new Font("ariel", 0, textSize));
 				for (UnitWME unit : game.getUnits()) {
 					g.drawString("" + unit.getID(), unit.getX()*tileSize, panelHeight  + unit.getY()*tileSize + textSize - 2);
-				}		
+				}
 			}
 
-/*			 
+/*
 			// Base Starting locations
 			g.setColor(Color.CYAN);
 			for (BaseLocationWME location : game.getBaseLocations()) {
-				g.fillRect(location.getX()*tileSize, panelHeight  + location.getY()*tileSize, 4*tileSize, 3*tileSize);				
+				g.fillRect(location.getX()*tileSize, panelHeight  + location.getY()*tileSize, 4*tileSize, 3*tileSize);
 			}
-			
+
 			// choke points
 			g.setColor(Color.PINK);
 			for (ChokePointWME location : game.getChokePoints()) {
 				int size = location.getWidth()/32;
 				System.out.println(size);
-				g.fillRect((location.getX() - size/2)*tileSize, 
-						panelHeight  + (location.getY()-size/2)*tileSize, size*tileSize, size*tileSize);				
+				g.fillRect((location.getX() - size/2)*tileSize,
+						panelHeight  + (location.getY()-size/2)*tileSize, size*tileSize, size*tileSize);
 			}
 */
-			
+
 		}
 		else {
 			int w = game.getMap().getMapWidth();
@@ -179,29 +191,29 @@ public class StarCraftFrame extends JPanel implements KeyListener {
 			HashMap<Integer, Double> playerInfluence = new HashMap<Integer, Double>();
 			HashMap<Integer, Double> enemyInfluence = new HashMap<Integer, Double>();
 			HashMap<Integer, Double> allyInfluence = new HashMap<Integer, Double>();
-					
+
 			for (UnitWME unit : game.getPlayerUnits()) {
 				for (int dy=-5; dy<=5; dy++) {
 					for (int dx=-5; dx<=5; dx++) {
 						double dist = Math.sqrt(dx*dx + dy*dy);
 						int x = unit.getX() + dx;
 						int y = unit.getY() + dy;
-						
+
 						Double influence = playerInfluence.get(y*w + x);
 						influence = influence != null ? influence : 0;
-						
+
 						if (dist == 0) {
-							influence += 0.5;							
+							influence += 0.5;
 						}
 						else if (dist > 0 && dist < 5) {
 							influence += 0.5/dist;
 						}
-						
+
 						influence = Math.min(1, influence);
 						playerInfluence.put(y*w + x, influence);
 					}
-				}					
-			}					
+				}
+			}
 
 			for (UnitWME unit : game.getEnemyUnits()) {
 				for (int dy=-5; dy<=5; dy++) {
@@ -209,22 +221,22 @@ public class StarCraftFrame extends JPanel implements KeyListener {
 						double dist = Math.sqrt(dx*dx + dy*dy);
 						int x = unit.getX() + dx;
 						int y = unit.getY() + dy;
-						
+
 						Double influence = enemyInfluence.get(y*w + x);
 						influence = influence != null ? influence : 0;
-						
+
 						if (dist == 0) {
-							influence += 0.5;							
+							influence += 0.5;
 						}
 						else if (dist > 0 && dist < 5) {
 							influence += 0.5/dist;
 						}
-						
+
 						influence = Math.min(1, influence);
 						enemyInfluence.put(y*w + x, influence);
 					}
-				}					
-			}					
+				}
+			}
 
 			for (UnitWME unit : game.getAllyUnits()) {
 				for (int dy=-5; dy<=5; dy++) {
@@ -232,23 +244,23 @@ public class StarCraftFrame extends JPanel implements KeyListener {
 						double dist = Math.sqrt(dx*dx + dy*dy);
 						int x = unit.getX() + dx;
 						int y = unit.getY() + dy;
-						
+
 						Double influence = allyInfluence.get(y*w + x);
 						influence = influence != null ? influence : 0;
-						
+
 						if (dist == 0) {
-							influence += 0.5;							
+							influence += 0.5;
 						}
 						else if (dist > 0 && dist < 5) {
 							influence += 0.5/dist;
 						}
-						
+
 						influence = Math.min(1, influence);
 						allyInfluence.put(y*w + x, influence);
 					}
-				}					
-			}					
-			
+				}
+			}
+
 			for (int y=0; y<h; y++) {
 				for (int x=0; x<w; x++) {
 					int position = y*w + x;
@@ -258,18 +270,18 @@ public class StarCraftFrame extends JPanel implements KeyListener {
 					pInfluence = (pInfluence != null) ? pInfluence : 0;
 					eInfluence = (eInfluence != null) ? eInfluence : 0;
 					aInfluence = (aInfluence != null) ? aInfluence : 0;
-					
+
 					g.setColor(new Color((float)(double)eInfluence, (float)(double)pInfluence, (float)(double)aInfluence, 0.2f));
-					g.fillRect(x*tileSize, panelHeight  + y*tileSize, tileSize, tileSize);									
+					g.fillRect(x*tileSize, panelHeight  + y*tileSize, tileSize, tileSize);
 				}
 			}
 		}
-		
+
 		// status panel
 		g.setColor(new Color(255,255,255));
 		g.setFont(new Font("ariel", 0, 12));
-		g.fillRect(0, 0, getWidth(), panelHeight); 
-		
+		g.fillRect(0, 0, getWidth(), panelHeight);
+
 		// minerals
 		g.setColor(new Color(0,0,255));
 		g.fillRect(5, 10, 10, 10);
@@ -285,11 +297,11 @@ public class StarCraftFrame extends JPanel implements KeyListener {
 		g.drawRect(105, 10, 10, 10);
 		g.setColor(new Color(0,0,0));
 		g.drawString("" + game.getPlayer().getGas(), 125, 20);
-		
+
 		// supply
 		g.setColor(new Color(0,0,0));
-		g.drawString((game.getPlayer().getSupplyUsed()/2) + "/" 
-  				   + (game.getPlayer().getSupplyTotal()/2), 200, 20);		
+		g.drawString((game.getPlayer().getSupplyUsed()/2) + "/"
+  				   + (game.getPlayer().getSupplyTotal()/2), 200, 20);
 	}
 
 	/**
@@ -305,3 +317,4 @@ public class StarCraftFrame extends JPanel implements KeyListener {
 
 	public void keyTyped(KeyEvent e) {}
 }
+
