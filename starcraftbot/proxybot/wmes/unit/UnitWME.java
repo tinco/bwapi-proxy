534bc4d8fff5370f6f73a53565aa9d525601a2e0
fischp@gmail.com <fischp@gmail.com@434c508a-d61b-11de-901b-a378632c6355>
2009-11-24 04:12:02 +0000

    added .equals to unit class
    git-svn-id: https://bwapi-proxy.googlecode.com/svn/trunk@36 434c508a-d61b-11de-901b-a378632c6355

--- starcraftbot/proxybot/wmes/unit/UnitWME.java
+++ starcraftbot/proxybot/wmes/unit/UnitWME.java
@@ -271,5 +271,27 @@ public class UnitWME {
 			" orderTimer:" + orderTimer +
 			" order:" + order + 
 			" resource:" + resources;
-	}
+		}
+
+	@Override
+ 	public int hashCode() {
+ 		final int prime = 31;
+ 		int result = 1;
+ 		result = prime * result + ID;
+ 		return result;
+ 	}
+
+ 	@Override
+ 	public boolean equals(Object obj) {
+ 		if (this == obj)
+ 			return true;
+ 		if (obj == null)
+ 			return false;
+ 		if (getClass() != obj.getClass())
+ 			return false;
+ 		UnitWME other = (UnitWME) obj;
+ 		if (ID != other.ID)
+ 			return false;
+ 		return true;
+ 	}
 }
